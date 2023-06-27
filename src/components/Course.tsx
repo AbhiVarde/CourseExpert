0e7a3b9c2a0a51b508eb67b2f2bb5079dc486e6d
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2023-06-27 22:21:40 +0530

    initial commit

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -58,21 +58,24 @@ const Course = () => {
 
   return (
     <div className="container p-4 max-w-xl bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition duration-300 ease-in-out mx-auto px-4 mt-8">
-      <div className="flex justify-between items-center my-4">
-        <h1 className="text-2xl font-bold">{selectedCourse.name}</h1>
-        <h1 className="text-2xl font-bold">${selectedCourse.price}</h1>
+      <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center my-4 gap-2">
+        <h1 className="text-xl sm:text-2xl font-bold sm:mb-0 sm:mr-4">
+          {selectedCourse.name}{" "}
+        </h1>
+        <h1 className="text-xl sm:text-2xl font-bold">
+          ${selectedCourse.price}
+        </h1>
       </div>
       <span className="text-gray-600">by {selectedCourse.by}</span>
       <p className="text-gray-600">Description: {selectedCourse.description}</p>
-      <h4 className="text-gray-600">Rating: {selectedCourse.rating}/5</h4>
+      <h4 className="text-gray-600">Rating: {selectedCourse.rating}</h4>
       <button
-        className={`bg-gray-800 hover:bg-gray-900 text-white py-2 px-4 rounded mt-4${
+        className={`bg-gray-800 hover:bg-gray-900 text-white py-2 px-4 rounded mt-4 ${
           isCelebrating ? "animate-pulse" : ""
-        }
-        `}
+        }`}
         onClick={handleButtonClick}
       >
-        {isClicked ? "Order Confirmed!" : "Buy Now!"}
+        ${isClicked ? "Purchased!" : "Buy Now!"}
       </button>
     </div>
   );
